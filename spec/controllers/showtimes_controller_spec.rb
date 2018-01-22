0209c4885e786ceb60bd9d90b228edbd1dc128fa
erumsanwari <erumsanwari@gmail.com>
2018-01-22 10:37:33 +0530

    Specs modified

--- spec/controllers/showtimes_controller_spec.rb
+++ spec/controllers/showtimes_controller_spec.rb
@@ -1,96 +1,94 @@
 require 'rails_helper'
 
 RSpec.describe ShowtimesController, type: :controller do
-	context 'Success' do
-		
-		context 'GET index' do
-			it 'should show all showtimes successfully' do
-	      get :index, format: 'json'
-	      response.should have_http_status(:ok)
-	    end
-	  end
-	  context 'GET show' do
-	    it 'should show showtime with given id successfully' do
-	      showtime = FactoryGirl.create(:showtime)
-	      get :show, id: showtime.id, format: 'json'
-	      response.should have_http_status(:ok)
-	    end
-	  end
-		context 'POST create' do
-			it 'should create a valid showtime' do
-        movie = FactoryGirl.create(:movie)
-				post :create, showtime: {time_of_show:"5:22", movie_id:movie.id},format: 'json'
-				response.should have_http_status(:ok)
-			end
-		end
-		context 'PUT update' do
-			it 'should update a valid showtime' do
-				showtime = FactoryGirl.create(:showtime)
-				put :update, id:showtime.id, showtime: {time_of_show:"4:55", movie_id:showtime.movie_id}, format: 'json'
-				new_showtime = Showtime.last
-        new_showtime.time_of_show.should eq "2000-01-01 04:55:00.000000000 +0000"
+  context 'Success' do
+    context 'GET index' do
+      it 'should show all showtimes successfully' do
+        get :index, format: 'json'
         response.should have_http_status(:ok)
-			end
-		end
-		context 'DELETE destroy' do
-			it 'should destroy a valid showtime' do
-				showtime = FactoryGirl.create(:showtime)
-				delete :destroy, id:showtime.id, format: 'json'
-				response.should have_http_status(:ok)
-			end
-		end
-	end
+      end
+    end
+    context 'GET show' do
+      it 'should show showtime with given id successfully' do
+        showtime = FactoryGirl.create(:showtime)
+        get :show, id: showtime.id, format: 'json'
+        response.should have_http_status(:ok)
+      end
+    end
+    context 'POST create' do
+      it 'should create a valid showtime' do
+        movie = FactoryGirl.create(:movie)
+        post :create, showtime: { time_of_show: '5:22', movie_id: movie.id }, format: 'json'
+        response.should have_http_status(:ok)
+      end
+    end
+    context 'PUT update' do
+      it 'should update a valid showtime' do
+        showtime = FactoryGirl.create(:showtime)
+        put :update, id: showtime.id, showtime: { time_of_show: '4:55', movie_id: showtime.movie_id }, format: 'json'
+        new_showtime = Showtime.last
+        new_showtime.time_of_show.strftime("%_H:%M").should eq ' 4:55'
+        response.should have_http_status(:ok)
+      end
+    end
+    context 'DELETE destroy' do
+      it 'should destroy a valid showtime' do
+        showtime = FactoryGirl.create(:showtime)
+        delete :destroy, id: showtime.id, format: 'json'
+        response.should have_http_status(:ok)
+      end
+    end
+  end
 
-	context'Failure' do
-
-		context 'GET show' do
-			it 'should not show a valid showtime' do
-				showtime = FactoryGirl.create(:showtime)
+  context 'Failure' do
+    context 'GET show' do
+      it 'should not show a valid showtime' do
+        showtime = FactoryGirl.create(:showtime)
         new_showtime = Showtime.last
-				get :show, id:new_showtime.id+1, format: 'json'
-				response.should have_http_status(:not_found)
-			end
-		end
-		context 'POST create' do
-			it 'should not create a showtime with invalid input' do
-				post :create, showtime: {time_of_show: "45645"},format: 'json'
-				response.should have_http_status(:unprocessable_entity)
-			end
-			it 'should not create a showtime with nil entries' do
-				post :create, showtime: {time_of_show:nil},format: 'json'
-				response.should have_http_status(:unprocessable_entity)
-			end
-			it 'should not create a showtime with invalid movie id' do
-				post :create, showtime: {time_of_show: "45645", movie_id:nil},format: 'json'
-				response.should have_http_status(:unprocessable_entity)
-			end
-		end
-		context 'PUT update' do
-			it 'should not update the showtime with invalid id' do
-				showtime = FactoryGirl.create(:showtime)
+        get :show, id: new_showtime.id + 1, format: 'json'
+        response.should have_http_status(:not_found)
+      end
+    end
+    context 'POST create' do
+      it 'should not create a showtime with invalid input' do
+        post :create, showtime: { time_of_show: '45645' }, format: 'json'
+        response.should have_http_status(:unprocessable_entity)
+      end
+      it 'should not create a showtime with nil entries' do
+        post :create, showtime: { time_of_show: nil }, format: 'json'
+        response.should have_http_status(:unprocessable_entity)
+      end
+      it 'should not create a showtime with invalid movie id' do
+        post :create, showtime: { time_of_show: '45645', movie_id: nil }, format: 'json'
+        response.should have_http_status(:unprocessable_entity)
+      end
+    end
+    context 'PUT update' do
+      it 'should not update the showtime with invalid id' do
+        showtime = FactoryGirl.create(:showtime)
         new_showtime = Showtime.last
-				put :update, id:new_showtime.id+1, showtime: {time_of_show:showtime.time_of_show}, format: 'json'
-				response.should have_http_status(:not_found)
-			end	
-			it 'should not update the showtime with invalid input' do
-				showtime = FactoryGirl.create(:showtime)
-				put :update, id:showtime.id, showtime: {time_of_show:nil}, format: 'json'
-				response.should have_http_status(:unprocessable_entity)
-			end	
-			it 'should not update the showtime with invalid movie id' do
-				showtime = FactoryGirl.create(:showtime)
+        put :update, id: new_showtime.id + 1, showtime: { time_of_show: showtime.time_of_show }, format: 'json'
+        response.should have_http_status(:not_found)
+      end
+      it 'should not update the showtime with invalid input' do
+        showtime = FactoryGirl.create(:showtime)
+        put :update, id: showtime.id, showtime: { time_of_show: nil }, format: 'json'
+        response.should have_http_status(:unprocessable_entity)
+      end
+      it 'should not update the showtime with invalid movie id' do
+        showtime = FactoryGirl.create(:showtime)
         new_showtime = Showtime.last
-				put :update, id:new_showtime.id+1, showtime: {movie_id:nil}, format: 'json'
-				response.should have_http_status(:not_found)
-			end
-		end 
-		context 'DELETE destroy' do
-			it 'should not destroy the showtime with invalid id' do
-				showtime = FactoryGirl.create(:showtime)
+        put :update, id: new_showtime.id + 1, showtime: { movie_id: nil }, format: 'json'
+        response.should have_http_status(:not_found)
+      end
+    end
+    context 'DELETE destroy' do
+      it 'should not destroy the showtime with invalid id' do
+        showtime = FactoryGirl.create(:showtime)
         new_showtime = Showtime.last
-				delete :destroy, id:new_showtime.id+1, format: 'json'
-				response.should have_http_status(:not_found)
-			end
-		end
-	end
+        delete :destroy, id: new_showtime.id + 1, format: 'json'
+        response.should have_http_status(:not_found)
+      end
+    end
+  end
 end
--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,110 +1,109 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
-	context 'Success' do
-		
-		context 'GET index' do
-			it 'should show all tickets successfully' do
-	      get :index, format: 'json'
-	      response.should have_http_status(:ok)
-	    end
-	  end
-	  context 'GET show' do
-	    it 'should show ticket with given id successfully' do
-	      ticket = FactoryGirl.create(:ticket)
-	      get :show, id: ticket.id, format: 'json'
-	      response.should have_http_status(:ok)
-	    end
-	  end
-		context 'POST create' do
-			it 'should create a valid ticket' do
+  context 'Success' do
+    context 'GET index' do
+      it 'should show all tickets successfully' do
+        get :index, format: 'json'
+        response.should have_http_status(:ok)
+      end
+    end
+    context 'GET show' do
+      it 'should show ticket with given id successfully' do
+        ticket = FactoryGirl.create(:ticket)
+        get :show, id: ticket.id, format: 'json'
+        response.should have_http_status(:ok)
+      end
+    end
+    context 'POST create' do
+      it 'should create a valid ticket' do
         showtime = FactoryGirl.create(:showtime)
         viewer = FactoryGirl.create(:viewer)
         booking = FactoryGirl.create(:booking)
-				post :create, ticket: { purchase_date: "01-02-2016", movie_date: "01-03-2016", price: 125, showtime_id:showtime.id, viewer_id:viewer.id, booking_id:booking.id }, format: 'json'
-				response.should have_http_status(:ok)
-			end
-		end
-		context 'PUT update' do
-			it 'should update a valid ticket' do
-				ticket = FactoryGirl.create(:ticket)
-				put :update, id:ticket.id, ticket: {purchase_date:ticket.purchase_date, movie_date:ticket.movie_date, price:240, viewer_id:ticket.viewer_id, booking_id:ticket.booking_id}, format: 'json'
-				new_ticket = Ticket.last
+        post :create, ticket: { purchase_date: '01-02-2016', movie_date: '01-03-2016', price: 125, showtime_id: showtime.id, viewer_id: viewer.id, booking_id: booking.id }, format: 'json'
+        response.should have_http_status(:ok)
+      end
+    end
+    context 'PUT update' do
+      it 'should update a valid ticket' do
+        ticket = FactoryGirl.create(:ticket)
+        put :update, id: ticket.id, ticket: { purchase_date: ticket.purchase_date, movie_date: "01-02-2017", price: 240, viewer_id: ticket.viewer_id, booking_id: ticket.booking_id }, format: 'json'
+        new_ticket = Ticket.last
+        new_ticket.movie_date.strftime("%d-%m-%Y").should eq "01-02-2017"
         new_ticket.price.should eq 240
         response.should have_http_status(:ok)
-			end
-		end
-		context 'DELETE destroy' do
-			it 'should destroy a valid ticket' do
-				ticket = FactoryGirl.create(:ticket)
-				delete :destroy, id:ticket.id, format: 'json'
-				response.should have_http_status(:ok)
-			end
-		end
-	end
+      end
+    end
+    context 'DELETE destroy' do
+      it 'should destroy a valid ticket' do
+        ticket = FactoryGirl.create(:ticket)
+        delete :destroy, id: ticket.id, format: 'json'
+        response.should have_http_status(:ok)
+      end
+    end
+  end
 
-	context'Failure' do
-
-		context 'GET show' do
-			it 'should not show a valid ticket' do
-				ticket = FactoryGirl.create(:ticket)
+  context 'Failure' do
+    context 'GET show' do
+      it 'should not show a valid ticket' do
+        ticket = FactoryGirl.create(:ticket)
         new_ticket = Ticket.last
-				get :show, id:new_ticket.id+1, format: 'json'
-				response.should have_http_status(:not_found)
-			end
-		end
-		context 'POST create' do
-			it 'should not create a ticket with invalid input' do
-				post :create, ticket: { movie_date:"abc" },format: 'json'
-				response.should have_http_status(:unprocessable_entity)
-			end
-			it 'should not create a ticket with nil entries' do
-				post :create, ticket: {purchase_date:nil},format: 'json'
-				response.should have_http_status(:unprocessable_entity)
-			end
-			it 'should not create a ticket with invalid viewer_id' do
-				post :create, ticket: {viewer_id:nil},format: 'json'
-				response.should have_http_status(:unprocessable_entity)
-			end
-			it 'should not create a ticket with invalid booking_id' do
+        get :show, id: new_ticket.id + 1, format: 'json'
+        response.should have_http_status(:not_found)
+      end
+    end
+    context 'POST create' do
+      it 'should not create a ticket with invalid input' do
+        post :create, ticket: { movie_date: 'abc' }, format: 'json'
+        response.should have_http_status(:unprocessable_entity)
+      end
+      it 'should not create a ticket with nil entries' do
+        post :create, ticket: { purchase_date: nil }, format: 'json'
+        response.should have_http_status(:unprocessable_entity)
+      end
+      it 'should not create a ticket with invalid viewer_id' do
+        post :create, ticket: { viewer_id: nil }, format: 'json'
+        response.should have_http_status(:unprocessable_entity)
+      end
+      it 'should not create a ticket with invalid booking_id' do
         booking = FactoryGirl.create(:booking)
         new_booking = Booking.last
-				post :create, ticket: {booking_id:new_booking.id+1},format: 'json'
-				response.should have_http_status(:unprocessable_entity)
-			end
-		end
-		context 'PUT update' do
-			it 'should not update the ticket with invalid id' do
-				ticket = FactoryGirl.create(:ticket)
+        post :create, ticket: { booking_id: new_booking.id + 1 }, format: 'json'
+        response.should have_http_status(:unprocessable_entity)
+      end
+    end
+    context 'PUT update' do
+      it 'should not update the ticket with invalid id' do
+        ticket = FactoryGirl.create(:ticket)
         new_ticket = Ticket.last
-				put :update, id:new_ticket.id+1, ticket: {purchase_date:ticket.purchase_date, movie_date:ticket.movie_date, price:ticket.price}, format: 'json'
-				response.should have_http_status(:not_found)
-			end	
-			it 'should not update the ticket with invalid input' do
-				ticket = FactoryGirl.create(:ticket)
-				put :update, id:ticket.id, ticket: {purchase_date:ticket.purchase_date, movie_date:nil}, format: 'json'
-				response.should have_http_status(:unprocessable_entity)
-			end	
-			it 'should not update the ticket with invalid viewer id' do
-				ticket = FactoryGirl.create(:ticket)
+        put :update, id: new_ticket.id + 1, ticket: { purchase_date: ticket.purchase_date, movie_date: ticket.movie_date, price: ticket.price }, format: 'json'
+        response.should have_http_status(:not_found)
+      end
+      it 'should not update the ticket with invalid input' do
+        ticket = FactoryGirl.create(:ticket)
+        put :update, id: ticket.id, ticket: { purchase_date: ticket.purchase_date, movie_date: nil }, format: 'json'
+        response.should have_http_status(:unprocessable_entity)
+      end
+      it 'should not update the ticket with invalid viewer id' do
+        ticket = FactoryGirl.create(:ticket)
         new_ticket = Ticket.last
-				put :update, id:new_ticket.id+1, ticket: {purchase_date:ticket.purchase_date, movie_date:ticket.movie_date, price:ticket.price, viewer_id:nil}, format: 'json'
-				response.should have_http_status(:not_found)
-			end
-			it 'should not update the ticket with invalid booking id' do
-				ticket = FactoryGirl.create(:ticket)
+        put :update, id: new_ticket.id + 1, ticket: { purchase_date: ticket.purchase_date, movie_date: ticket.movie_date, price: ticket.price, viewer_id: nil }, format: 'json'
+        response.should have_http_status(:not_found)
+      end
+      it 'should not update the ticket with invalid booking id' do
+        ticket = FactoryGirl.create(:ticket)
         new_ticket = Ticket.last
-				put :update, id:new_ticket.id+1, ticket: {purchase_date:ticket.purchase_date, movie_date:ticket.movie_date, price:ticket.price, booking_id:nil}, format: 'json'
-				response.should have_http_status(:not_found)
-			end
-		end 
-		context 'DELETE destroy' do
-			it 'should not destroy the ticket with invalid id' do
-				ticket = FactoryGirl.create(:ticket)
+        put :update, id: new_ticket.id + 1, ticket: { purchase_date: ticket.purchase_date, movie_date: ticket.movie_date, price: ticket.price, booking_id: nil }, format: 'json'
+        response.should have_http_status(:not_found)
+      end
+    end
+    context 'DELETE destroy' do
+      it 'should not destroy the ticket with invalid id' do
+        ticket = FactoryGirl.create(:ticket)
         new_ticket = Ticket.last
-				delete :destroy, id:new_ticket.id+1, format: 'json'
-				response.should have_http_status(:not_found)
-			end
-		end
-	end
+        delete :destroy, id: new_ticket.id + 1, format: 'json'
+        response.should have_http_status(:not_found)
+      end
+    end
+  end
 end
